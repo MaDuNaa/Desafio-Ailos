c7ea9ee915501283c1b12019162a7f6d12f142bc
Matheus Nascimento <ma_eduard@hotmail.com>
2023-03-02 22:05:32 -0300

    Criando pagina de Consulta de CPF , ajustes no codigo em Nav e Header, e Criando o component Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
+import { ConsultaCpfComponent } from './views/consulta-cpf/consulta-cpf.component';
 
                     //MATERIAL
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavComponent,
+    ConsultaCpfComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,6 +31,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatSidenavModule,
     MatToolbarModule,
     BrowserAnimationsModule,
+    MatInputModule,
     
 
   ],
---
+++ src/app/views/consulta-cpf/consulta-cpf.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-cpf',
+  templateUrl: './consulta-cpf.component.html',
+  styleUrls: ['./consulta-cpf.component.css']
+})
+export class ConsultaCpfComponent {
+
+}
